a0f8575f9bf783c303d27f8f7a42702d91005e1a
Richo-Worku <raheldemissie56@gmail.com>
2022-11-02 14:42:40 +0300

    basic changes

---
+++ lib/screens/fifthscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FifthPage extends StatefulWidget {
+  const FifthPage({super.key});
+
+  @override
+  State<FifthPage> createState() => _FifthPageState();
+}
+
+class _FifthPageState extends State<FifthPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
